cdf29299d2206db315eadf8e9b6799543d782f96
zd4y <ariasorozco03@gmail.com>
2020-01-07 09:17:04 -0500

    Add color to help <command> embed

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ async def help(ctx, command=None):
             aliases = ', '.join(cmd.aliases)
         embed = Embed(
             title=f'Ayuda sobre el comando {cmd.name}',
-            description=f'{cmd.help}\nAlias: {aliases}\nUso: `{cmd.usage}`'
+            description=f'{cmd.help}\nAlias: {aliases}\nUso: `{cmd.usage}`',
+            color=Color.red()
         )
         await ctx.send(embed=embed)
     else:
