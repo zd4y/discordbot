5fb4d4e4b5caf050e550e4e4e281674d47706bc0
zd4y <ariasorozco03@gmail.com>
2020-01-08 12:20:37 -0500

    Increase string length in the Setting table

--- db.py
+++ db.py
@@ -24,7 +24,7 @@ class Setting(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(50), nullable=False)
-    value = Column(String(50), nullable=False)
+    value = Column(String(500), nullable=False)
     guild_id = Column(BigInteger, ForeignKey('guilds.id'))
 
 
