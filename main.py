5ef3d4d42ade6859a19a29dc13071305758bffed
zd4y <ariasorozco03@gmail.com>
2020-01-13 21:46:54 -0500

    Fix get_prefix v3

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from config import Config, ServerConfig
 
 
 async def get_prefix(bot, msg):
-    return await ServerConfig.get_setting(msg.guild.id, 'prefix').split()
+    prefix = await ServerConfig.get_setting(msg.guild.id, 'prefix')
+    return prefix.split()
 
 
 bot = commands.Bot(command_prefix=get_prefix)
