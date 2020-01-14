17e316e1887285778e36660a64841a6a88e9064e
zd4y <ariasorozco03@gmail.com>
2020-01-13 20:25:07 -0500

    Fix problem in get_prefix v2

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from discord.ext import commands
 from config import Config, ServerConfig
 
 
-def get_prefix(bot, msg):
-    prefix = await ServerConfig.get_setting(msg.guild.id, 'prefix').split()
-    return prefix
+async def get_prefix(bot, msg):
+    return await ServerConfig.get_setting(msg.guild.id, 'prefix').split()
 
 
 bot = commands.Bot(command_prefix=get_prefix)
