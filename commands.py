9b5a4a92200904452ea085ab751a9ff31eef8049
zd4y <ariasorozco03@gmail.com>
2020-01-17 20:26:45 -0500

    Add logging.StramHandler

--- commands.py
+++ commands.py
@@ -9,6 +9,9 @@ import logging
 
 logging.basicConfig(filename='./log.txt', level=logging.DEBUG,
                     format='%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logging.getLogger('').addHandler(console)
 
 
 async def fetch(session, url, **kwargs):
